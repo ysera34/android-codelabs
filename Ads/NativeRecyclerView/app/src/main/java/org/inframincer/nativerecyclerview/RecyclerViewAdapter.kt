5e4c07033fc5e85f8551d2c4e0190b4b0c685147
ysera34 <ycyoon34@gmail.com>
2019-03-24 11:59:22 +0900

    AdMob Native Advanced Ads: Populate the asset views

--- Ads/NativeRecyclerView/app/src/main/java/org/inframincer/nativerecyclerview/RecyclerViewAdapter.kt
+++ Ads/NativeRecyclerView/app/src/main/java/org/inframincer/nativerecyclerview/RecyclerViewAdapter.kt
@@ -5,9 +5,12 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import com.google.android.gms.ads.formats.UnifiedNativeAd
+import com.google.android.gms.ads.formats.UnifiedNativeAdView
 
 // A menu item view type.
 private const val MENU_ITEM_VIEW_TYPE = 0
@@ -15,8 +18,8 @@ private const val MENU_ITEM_VIEW_TYPE = 0
 // The unified native ad view type.
 private const val UNIFIED_NATIVE_AD_VIEW_TYPE = 1
 
-class RecyclerViewAdapter(val context: Context, val recyclerViewItems: List<Any>)
-    : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class RecyclerViewAdapter(val context: Context, val recyclerViewItems: List<Any>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     /**
      * The [MenuItemViewHolder] class.
@@ -65,8 +68,9 @@ class RecyclerViewAdapter(val context: Context, val recyclerViewItems: List<Any>
             }
             else -> { // include MENU_ITEM_VIEW_TYPE
                 val menuItemLayoutView = LayoutInflater.from(viewGroup.context)
-                    .inflate(R.layout.menu_item_container, viewGroup, false
-                )
+                    .inflate(
+                        R.layout.menu_item_container, viewGroup, false
+                    )
                 MenuItemViewHolder(menuItemLayoutView)
             }
         }
@@ -91,5 +95,53 @@ class RecyclerViewAdapter(val context: Context, val recyclerViewItems: List<Any>
         menuItemHolder.menuItemPrice.text = price
         menuItemHolder.menuItemCategory.text = category
         menuItemHolder.menuItemImage.setImageResource(imageResID)
+    }
+
+    private fun populateNativeAdView(nativeAd: UnifiedNativeAd, adView: UnifiedNativeAdView) {
+        // Some assets are guaranteed to be in every UnifiedNativeAd.
+        (adView.headlineView as TextView).text = nativeAd.headline
+        (adView.bodyView as TextView).text = nativeAd.body
+        (adView.callToActionView as Button).text = nativeAd.callToAction
+
+        // These assets aren't guaranteed to be in every UnifiedNativeAd,
+        // so it's important to check before trying to display them.
+        val icon = nativeAd.icon
+        if (icon == null) {
+            adView.iconView.visibility = View.INVISIBLE
+        } else {
+            (adView.iconView as ImageView).setImageDrawable(icon.drawable)
+            adView.iconView.visibility = View.VISIBLE
+        }
+
+        if (nativeAd.price == null) {
+            adView.priceView.visibility = View.INVISIBLE
+        } else {
+            adView.priceView.visibility = View.VISIBLE
+            (adView.priceView as TextView).text = nativeAd.price
+        }
+
+        if (nativeAd.store == null) {
+            adView.storeView.visibility = View.INVISIBLE
+        } else {
+            adView.storeView.visibility = View.VISIBLE
+            (adView.storeView as TextView).text = nativeAd.store
+        }
+
+        if (nativeAd.starRating == null) {
+            adView.starRatingView.visibility = View.INVISIBLE
+        } else {
+            (adView.starRatingView as RatingBar).rating = nativeAd.starRating.toFloat()
+            adView.starRatingView.visibility = View.VISIBLE
+        }
+
+        if (nativeAd.advertiser == null) {
+            adView.advertiserView.visibility = View.INVISIBLE
+        } else {
+            (adView.advertiserView as TextView).text = nativeAd.advertiser
+            adView.advertiserView.visibility = View.VISIBLE
+        }
+
+        // Assign native ad object to the native view.
+        adView.setNativeAd(nativeAd)
     }
 }
